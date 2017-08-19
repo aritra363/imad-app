1ddc5a97c3a7495297d33f85d5ab373eb9588c83
aritra363 <30936415+aritra363@users.noreply.github.com>
2017-08-20 00:25:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/article1', function (req, res) {
-  res.sendFile(path.join(__dirname,'ui','main.html'));
+  res.sendFile(path.join(__dirname,'ui','art.html'));
 });
 app.get('/ui/st.css', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','st.css'));
