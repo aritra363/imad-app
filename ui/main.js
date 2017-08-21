a9c65c78782e5d5899d2a1e57a4854b8b8a10c6e
aritra363 <30936415+aritra363@users.noreply.github.com>
2017-08-21 15:05:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,24 @@
-var counter=0;
+
 var butt=document.getElementById('count');
 butt.onclick =function ()
 {
-  counter=counter+1;
-  var span=document.getElementById('c');
-  span.innerHTML=counter.toString();
+    var req=new XMLHttpRequests();
+    req.onreadystatechange= function()
+    { if(req.readystate===XMLHttpRequest.DONE)
+    {
+        if (req.status===200)
+        {
+          var counter=req.responseText;
+          var span=document.getElementById('c');
+          span.innerHTML=counter.toString(); 
+        }
+    }
+        
+    }
+    //make request
+    req.open('Get','http://aritrapaul363.imad.hasura-app.io/counter');
+    req.send('null');
+  
+  
   
 }
