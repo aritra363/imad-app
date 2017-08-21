8056488622906a4e3745d8cd6048de938b169f4d
aritra363 <30936415+aritra363@users.noreply.github.com>
2017-08-21 13:05:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,13 @@ console.log('Loaded!');
 alert("Hi!bye");
 //move image
 var img=document.getElementById('dice');
-img.onclick = function ()
-{
-    function right()
-    {   var ml=ml+10;
+ function right()
+    {   var ml=0;
+         ml=ml+10;
         img.style.marginLeft=ml+'px';
     }
+img.onclick = function ()
+{
+   
     var inter=setInterval(right,100);
 }
