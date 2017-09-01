be759123fb99478a90590151e018c8b2974b52ea
aritra363 <30936415+aritra363@users.noreply.github.com>
2017-09-01 12:29:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get ('/counter',function(req,res){
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-var pool=new pool(config);
+var pool=new Pool(config);
 app.get('/test-db', function (req, res) {
 pool.query('select * from test',function(err,result)
 {
